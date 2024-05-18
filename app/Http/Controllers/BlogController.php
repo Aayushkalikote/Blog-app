c4a733db921718686907620bfb19652accf6f752
Aayushkalikote <aayushkalikote31@gmail.com>
2024-05-18 19:36:30 +0545

    backend handled

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('CreateBlog');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'author' => 'required',
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        $blog = Blog::create($validated);
+
+        return redirect()->route('welcome')->with('success', 'Blog post created successfully!');
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Blog $blog)
+    {
+        //
+    }
+}
