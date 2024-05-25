24755cf81a448c26d0d13f27a5a629dbfe371a4a
Aayushkalikote <aayushkalikote31@gmail.com>
2024-05-25 11:18:00 +0545

    store blogs backenmd

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+     /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        // Validate the incoming request data
+        $validator = Validator::make($request->all(),[
+            'author' => 'required',
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Error.',
+                'errors' => $validator->errors()
+            ]);
+        }else{
+            $blog = new Blog();
+            $blog->author = $request->input('author');
+            $blog->title = $request->input('title');
+            $blog->description = $request->input('description');
+            $blog->save();
+            return response()->json([
+                'status' => true,
+                'message' => 'Blog post created successfully!',
+                'data' => $blog
+            ]);
+        }
+
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Blog $blog)
+    {
+        //
+    }
+}
